e4a4e30e89f988d2b14197f21f64d446c579293b
ooo3345sjh <111489860+ooo3345sjh@users.noreply.github.com>
2022-12-12 07:43:03 +0900

    2022/12/12 이미지 수집완료 및 이미지 자동 업로드 기능 구현 (#46)

---
+++ Kmarket/src/main/java/kr/co/Kmarket/utils/autoUpload/XmlVO.java
@@ -0,0 +1,61 @@
+package kr.co.Kmarket.utils.autoUpload;
+
+public class XmlVO {
+	private int NO;
+	private String company;
+	private String title;
+	private int price;
+	private String thumb1;
+	private String thumb2;
+	private String thumb3;
+	private String detail;
+	
+	public String getThumb1() {
+		return thumb1;
+	}
+	public void setThumb1(String thumb1) {
+		this.thumb1 = thumb1;
+	}
+	public String getThumb2() {
+		return thumb2;
+	}
+	public void setThumb2(String thumb2) {
+		this.thumb2 = thumb2;
+	}
+	public String getThumb3() {
+		return thumb3;
+	}
+	public void setThumb3(String thumb3) {
+		this.thumb3 = thumb3;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+	public int getNO() {
+		return NO;
+	}
+	public void setNO(int nO) {
+		NO = nO;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
