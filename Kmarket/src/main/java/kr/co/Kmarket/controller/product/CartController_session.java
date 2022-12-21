1b187196e78852b8b2fbc10462fe1af6ba624ba0
ooo3345sjh <ooo3345sjh@gamil.com>
2022-12-21 19:39:47 +0900

    2022/12/21 장바구니 및 주문 페이지 구현중

--- Kmarket/src/main/java/kr/co/Kmarket/controller/product/CartController_session.java
+++ Kmarket/src/main/java/kr/co/Kmarket/controller/product/CartController_session.java
@@ -24,8 +24,8 @@ import kr.co.Kmarket.service.ProductService;
 import kr.co.Kmarket.vo.CartVo;
 import kr.co.Kmarket.vo.ProductVO;
 
-//@WebServlet("/product/cart.do")
-public class CartController extends HttpServlet {
+@WebServlet("/product/cart.do")
+public class CartController_session extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	private ProductService service = new ProductService();
@@ -42,14 +42,21 @@ public class CartController extends HttpServlet {
 		List<CartVo> list = service.selectProductInCart(uid);
 		
 		req.setAttribute("request", req);
+		req.setAttribute("session", req.getSession());
 		req.setAttribute("list", list);
-		req.getRequestDispatcher("/product/cart.jsp").forward(req, resp);
+		req.getRequestDispatcher("/product/cartSess.jsp").forward(req, resp);
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		logger.info("CartController doPost...");
 		
+		
+		String[] carts = req.getParameterValues("장바구니 번호들");
+		
+		List<CartVo> list = null;
+		
+		req.getSession().setAttribute("list", list);
 		
 	}
 
