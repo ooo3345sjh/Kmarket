86d9cf270b22846010672aa70b764e65155bc3b4
JGwanghou <111489868+JGwanghou@users.noreply.github.com>
2022-12-26 14:50:29 +0900

    J gwanghou (#155)
    * 더보기 구현중
    
    * admin/cs 공지사항 목록, 보기, 작성 HTML 구현
    
    * 2022/12/26 최신화

--- Kmarket/src/main/java/kr/co/Kmarket/controller/admin/_CsWriteController.java
+++ Kmarket/src/main/java/kr/co/Kmarket/controller/admin/_CsWriteController.java
@@ -1,6 +1,7 @@
 package kr.co.Kmarket.controller.admin;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,21 +10,50 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.co.Kmarket.service.CsService;
+import kr.co.Kmarket.vo.CsVO;
+
 @WebServlet("/admin/cs/write.do")
 public class _CsWriteController extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
-
+	private CsService service = new CsService();
+	
 	@Override
 	public void init() throws ServletException {
 	}
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String cate1 = req.getParameter("cate1");
+		
+		req.setAttribute("cate1", cate1);
 		
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/admin/cs/write.jsp");
 		dispatcher.forward(req, resp);
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String uid	 = req.getParameter("uid");
+		String cate1 = req.getParameter("cate1");
+		String type	 = req.getParameter("type");
+		String title = req.getParameter("title");
+		String content = req.getParameter("content");
+		String regip = req.getRemoteAddr();
+		
+		CsVO cvo = new CsVO();
+		cvo.setUid(uid);
+		cvo.setCate1(cate1);
+		cvo.setType(type);
+		cvo.setTitle(title);
+		cvo.setContent(content);
+		cvo.setRegip(regip);
+		
+		int result = service.insertAdminNoticeArticle(cvo);
+		
+		if(result > 0) {
+			resp.sendRedirect("/Kmarket/admin/cs/list.do?cate1="+cate1);
+		}else {
+			
+		}
 	}
 }
---
+++ Kmarket/src/main/webapp/admin/js/csArticle.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+ 
+ $(function(){
+	
+	$('select[name=type]').change(function(){
+			console.log($('select[name=type] option:selected').text());
+	})
+	
+	$(document).on('click', '.remove', function(e){
+		e.preventDefault();
+		
+		console.log('test1');
+		let isDeleteOk = confirm("정말 삭제하시겠습니까?");
+		
+		if(isDeleteOk){
+			let article = $(this).closest('tr');
+			let csNo = $(this).attr('data-no');
+			
+			$.ajax({
+				url: '/Kmarket/admin/cs/delete.do',
+				type: 'get',
+				data: {'csNo':csNo},
+				dataType: 'json',
+				success: function(data){
+					if(data.result > 0){
+						alert('게시글이 삭제되었습니다.');
+						article.hide();
+					}
+				}
+			});
+		}
+		
+	});
+})
+
